751dbc54b1e166d5eac28bd26a60b2cd97aa078c
dinarwijaksono11 <dinarwijaksono11@gmail.com>
2024-02-10 15:51:50 +0700

    chapter 001 - 3

--- src/Chapter_001.php
+++ src/Chapter_001.php
@@ -16,4 +16,25 @@ class Chapter_001
 
         return $result;
     }
+
+    // 2 - 
+    // https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+    public static function reverseSeq(int $n): array
+    {
+        $result = [];
+
+        for ($i = $n; $i > 0; $i--) {
+            $result[] = $i;
+        }
+
+        return $result;
+    }
+
+
+    // 3
+    // https://www.codewars.com/kata/59ca8246d751df55cc00014c
+    public static function hero(int $bullets, int $dragons): bool
+    {
+        return $bullets >= $dragons * 2;
+    }
 }
--- tests/Chapter_001Test.php
+++ tests/Chapter_001Test.php
@@ -13,4 +13,23 @@ class Chapter_001Test extends TestCase
         $this->assertEquals(16, Chapter_001::grow([4, 1, 1, 1, 4]));
         $this->assertEquals(64, Chapter_001::grow([2, 2, 2, 2, 2, 2]));
     }
+
+
+    function test_reverseSeq()
+    {
+        $this->assertSame([5, 4, 3, 2, 1], Chapter_001::reverseSeq(5));
+        $this->assertSame([3, 2, 1], Chapter_001::reverseSeq(3));
+        $this->assertSame([1], Chapter_001::reverseSeq(1));
+    }
+
+
+    function test_hero()
+    {
+        $this->assertEquals(true, Chapter_001::hero(10, 5));
+        $this->assertEquals(false, Chapter_001::hero(7, 4));
+        $this->assertEquals(false, Chapter_001::hero(4, 5));
+        $this->assertEquals(true, Chapter_001::hero(100, 40));
+        $this->assertEquals(false, Chapter_001::hero(1500, 751));
+        $this->assertEquals(false, Chapter_001::hero(0, 1));
+    }
 }
